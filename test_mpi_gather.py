e735c27a093c8ce79ad9f0f56affe6521089d0ec
Khee-Gan Lee <kglee@lbl.gov>
2017-06-29 17:06:18 -0700

    - test_mpi_gather.py is functional: scatters and gathers array elements in MPI - BootCovar_mpi.py now works on cori - Added boostrap_covar_convergence.ipynb to plot convergence tests

--- test_mpi_gather.py
+++ test_mpi_gather.py
@@ -2,18 +2,8 @@
 Test scattering and gathering a multi-dimensional array in MPI.
 """
 
-import sys
 import numpy as np
-import copy
-import lyafxcorr_kg as xcorr
-import time
 from mpi4py import MPI
-from astropy.cosmology import FlatLambdaCDM
-from astropy.io import fits
-from astropy.io import ascii
-from astropy.table import Table
-from astropy import units as u
-from astropy.coordinates import SkyCoord
 
 comm = MPI.COMM_WORLD
 rank = comm.Get_rank()
@@ -21,21 +11,25 @@ size = comm.Get_size()
 
 
 if rank == 0:
-    # Read in sample of realizations, and rearrange indices so that the
-    #iterations are in the first row
-    fullarr_in   = np.load("covar_mosdef_n400_1.npy")
+    print("Reading in: bootsamp_mosdef_n400_1.npy")
+    fullarr_in   = np.load("bootsamp_mosdef_n400_1.npy")
+    print(np.shape(fullarr_in))
     fullarr      = np.rollaxis(fullarr_in, 2,0)
     # Flatten
     fullarr_flat = np.ndarray.flatten(fullarr)
-    nvec_percore    = len(fullarr_flat)/size
+    nvec_percore    = np.array(len(fullarr_flat)/size,'int')
     gathered_flat = np.empty(len(fullarr_flat))
+    print("{:} elements per core".format(nvec_percore))
 else:
-    fullarr      = None
+    fullarr_in = None
     fullarr_in   = None
     fullarr_flat = None
+    gathered_flat = None
     nvec_percore = None
 
-comm.Bcast(nvec_percore, root=0) 
+nvec_percore = comm.bcast(nvec_percore, root=0) 
+
+print("Rank {0:} is receiving {1:} elements".format(rank,nvec_percore))
 
 arr_local = np.zeros(nvec_percore)
 
@@ -43,12 +37,11 @@ comm.Scatter(fullarr_flat, arr_local, root=0)
 
 comm.Barrier()
 
-gathered_flat = comm.gather(arr_local, root=0)
+comm.Gather(arr_local, gathered_flat, root=0)
 
 if rank == 0:
     gathered_reshape = np.reshape(gathered_flat, np.shape(fullarr))
     full_gathered = np.rollaxis(gathered_reshape, 1,0)
     full_gathered = np.rollaxis(full_gathered, 2,1)
-    np.array_equal(
-
-exit()
+    np.array_equal(full_gathered, fullarr_in)
+    np.save("bootsamp_test_out_mosdef_n400_1.npy", full_gathered)
