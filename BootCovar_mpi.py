5ae16f2c6c854636cb957c7d6c583221dc24a1d5
Khee-Gan Lee <kglee@lbl.gov>
2017-06-29 17:06:18 -0700

    - test_mpi_gather.py is functional: scatters and gathers array elements in MPI - BootCovar_mpi.py now works on cori

--- BootCovar_mpi.py
+++ BootCovar_mpi.py
@@ -1,11 +1,11 @@
 """
 Estimate covariance matrix of galaxy-Lya forest cross-correlations via 
-bootstrap. 
+bootstrap. This is a MPI version.
 
 Takes an argument that points to an ASCII config file. 
 
 Run as:
-> python BootCovar_script.py input.cfg
+> python_mpi BootCovar_mpi.py input.cfg
 """
 
 import sys
@@ -23,93 +23,135 @@ from astropy.coordinates import SkyCoord
 
 comm = MPI.COMM_WORLD
 rank = comm.Get_rank()
+size = comm.Get_size()
 
-t0 = time.clock()
+if rank==0:
+    t0 = time.clock()
 
-# Define cosmology
-cosmo = FlatLambdaCDM(H0=70, Om0=0.31)
+    # Define cosmology
+    cosmo = FlatLambdaCDM(H0=70, Om0=0.31)
+    # Open config file and parse parameters
+    input_fil = str(sys.argv[1]) 
 
-# Open config file and parse parameters
-input_fil = str(sys.argv[1]) 
+    f=open(input_fil, 'r')
+    lines = f.readlines()
+    f.close()
 
-f=open(input_fil, 'r')
-lines = f.readlines()
-f.close()
+    pixfil     = lines[0].partition('#')[0].rstrip()
+    galfil     = lines[1].partition('#')[0].rstrip()
+    cat_str    = lines[2].partition('#')[0].rstrip()
+    PiBin_fil  = lines[3].partition('#')[0].rstrip()
+    SigBin_fil = lines[4].partition('#')[0].rstrip()
+    nsamp      = np.int(lines[5].partition('#')[0].rstrip())
+    randseed_in= np.int(lines[6].partition('#')[0].rstrip())
+    outsuffix  = lines[7].partition('#')[0].rstrip()
 
-pixfil     = lines[0].partition('#')[0].rstrip()
-galfil     = lines[1].partition('#')[0].rstrip()
-cat_str    = lines[2].partition('#')[0].rstrip()
-PiBin_fil  = lines[3].partition('#')[0].rstrip()
-SigBin_fil = lines[4].partition('#')[0].rstrip()
-nsamp      = np.int(lines[5].partition('#')[0].rstrip())
-randseed_in= np.int(lines[6].partition('#')[0].rstrip())
-outsuffix  = lines[7].partition('#')[0].rstrip()
+    # Read in forest pixels
+    LyaPix = xcorr.lyapix(pixfil,cosmo=cosmo)
+    print("Read in {0:d} Ly-a forest pixels".format(LyaPix.npix))
+    npix = LyaPix.npix
+
+    # Read in galaxy positions
+    gal_table = ascii.read(galfil)
+    gal = gal_table[gal_table['source'] == cat_str]
+    print('Read in {0:d} galaxies'.format(len(gal)))
+
+    # Generate 3D sky positions for galaxies
+    GalCoords = SkyCoord(ra=gal['ra']*u.degree,dec=gal['dec']*u.degree,
+                         distance=cosmo.comoving_distance(gal['zspec']))
+    ngal = len(GalCoords)
+
+    # Read in bin edges
+    PiBins0 = ascii.read(PiBin_fil)
+    SigBins0 = ascii.read(SigBin_fil)
+
+    PiEdges = PiBins0['pi_edges'].data
+    SigEdges = SigBins0['sigma_edges'].data
+
+    # Convert bin boundaries from Mpc/h to Mpc
+    PiEdges  = PiEdges/(len(PiEdges)*[cosmo.h])
+    SigEdges = SigEdges/(len(SigEdges)*[cosmo.h])
+    PiBound = (min(PiEdges), max(PiEdges))
+
+    # Conformability test
+    if (nsamp % size != 0):
+        print("The number of cores must evenly divide nsamp")
+        comm.Abort()
+
+    # Number of bootstrap samples per node
+    nsamp_node = np.int(nsamp/ size)
+
+    # This is the array to receive gathered samples
+    XCorrSamples_flat = np.empty(nsamp*(len(SigEdges)-1)*(len(PiEdges)-1))
+else:
+    LyaPix = None
+    GalCoords = None
+    ngal = None
+    nsamp_node = None
+    SigEdges = None
+    PiEdges = None
+    randseed_in= None
+    cosmo = None
+    XCorrSamples_flat = None
+
+cosmo  = comm.bcast(cosmo, root=0)
+LyaPix = comm.bcast(LyaPix,root=0)
+GalCoords = comm.bcast(GalCoords,root=0)
+ngal = comm.bcast(ngal,root=0)
+nsamp_node = comm.bcast(nsamp_node, root=0)
+SigEdges = comm.bcast(SigEdges, root=0)
+PiEdges = comm.bcast(PiEdges, root=0)
+randseed_in = comm.bcast(randseed_in, root=0)
 
 # If RANDSEED_IN is positive, use it to initialize random number
 # generator. Negative values will be ignored
-
+#
+# Results are reproducible only if input seed, rank AND 
+# nsamp are the same!
 if randseed_in > 0:
-    np.random.seed(seed=randseed_in)
-
-# Read in forest pixels
-LyaPix = xcorr.lyapix(pixfil,cosmo=cosmo)
-print("Read in {0:d} Ly-a forest pixels".format(LyaPix.npix))
-npix = LyaPix.npix
-
-# Read in galaxy positions
-gal_table = ascii.read(galfil)
-gal = gal_table[gal_table['source'] == cat_str]
-print('Read in {0:d} galaxies'.format(len(gal)))
-
-# Generate 3D sky positions for galaxies
-GalCoords = SkyCoord(ra=gal['ra']*u.degree,
-                 dec=gal['dec']*u.degree,
-                 distance=cosmo.comoving_distance(
-                     gal['zspec']))
-
-# Read in bin edges
-PiBins0 = ascii.read(PiBin_fil)
-SigBins0 = ascii.read(SigBin_fil)
-
-PiEdges = PiBins0['pi_edges'].data
-SigEdges = SigBins0['sigma_edges'].data
-
-# Convert bin boundaries from Mpc/h to Mpc
-PiEdges  = PiEdges/(len(PiEdges)*[cosmo.h])
-SigEdges = SigEdges/(len(SigEdges)*[cosmo.h])
-PiBound = (min(PiEdges), max(PiEdges))
-
-# Initiate Bootstrap
-ngal = len(GalCoords)
+    np.random.seed(seed=randseed_in+rank)
 
 # Initialize output array to store all the bootstrap samples
-XCorrSamples = np.empty([len(SigEdges)-1, len(PiEdges)-1, nsamp])
+# This has the realizations stored in the leading dimension, which will
+# need to be rolled before saving.
+XCorrSamples_local = np.empty([nsamp_node, len(SigEdges)-1, len(PiEdges)-1])
 
 # This is the loop over the desired resamples
-for ii in range(0,nsamp):
-    if (ii % 5) == 0:
-        print("Iteration #", ii)
+for ii in range(0,nsamp_node):
+    if (rank == 0) and (ii % 5 == 0):
+        print("Iteration {} on rank 0".format(ii))
     # Make a copy of the pixels and resample
     LyaPixTmp = copy.deepcopy(LyaPix)
     LyaPixTmp = LyaPixTmp.resample()
     # Resample galaxy positions
     GalCoordTmp = GalCoords[np.random.choice(ngal,ngal,replace=True)]
     XCorrTmp, _ = xcorr.xcorr_gal_lya(GalCoordTmp, LyaPixTmp,SigEdges, PiEdges,
-                                      cosmo=cosmo,verbose=0)
-    XCorrSamples[:,:,ii] = XCorrTmp
+                                          cosmo=cosmo,verbose=0)
+    XCorrSamples_local[ii,:,:] = XCorrTmp
 
-# First, reshape the bootstrap samples so that it has dimensions (nbin, nsamp)
-XBootReshaped = XCorrSamples.reshape(-1, XCorrSamples.shape[-1])
-Covar = np.cov(XBootReshaped)
+XCorrSamples_local_flat = np.ndarray.flatten(XCorrSamples_local)
+comm.Barrier()
 
-outfil_cov = 'covar_'+outsuffix+'.npy'
-outfil_samp = 'bootsamp_'+outsuffix+'.npy'
+comm.Gather(XCorrSamples_local_flat, XCorrSamples_flat, root=0)
 
-np.save(outfil_cov, Covar)
-np.save(outfil_samp, XCorrSamples)
+if rank == 0:
+    # Unflatten
+    XCorrSamples = np.reshape(XCorrSamples_flat,
+                              [nsamp, len(SigEdges)-1, len(PiEdges)-1])
+    # Reshape to original shape for boostrap realizations
+    XCorrSamples = np.rollaxis(XCorrSamples, 1, 0)
+    XCorrSamples = np.rollaxis(XCorrSamples, 2, 1)
+    # First, reshape the bootstrap samples so that it has dimensions (nbin, nsamp)
+    XBootReshaped = XCorrSamples.reshape(-1, XCorrSamples.shape[-1])
+    Covar = np.cov(XBootReshaped)
 
-t1 = time.clock()
+    outfil_cov = 'covar_'+outsuffix+'.npy'
+    outfil_samp = 'bootsamp_'+outsuffix+'.npy'
 
-print('Total runtime (s) = {}'.format(t1-t0))
+    np.save(outfil_cov, Covar)
+    np.save(outfil_samp, XCorrSamples)
+
+    t1 = time.clock()
+    print('Total runtime (s) = {}'.format(t1-t0))
 
 exit()
